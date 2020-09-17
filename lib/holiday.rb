f6e54d952ec6b72ea80bcc9771a487f437e3c71d
Devin Cooper <dcoope12@asu.edu>
2020-09-17 05:02:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |season, holidays|
+     
+  holidays.each do|supplies, value|
+    "#{supplies}: #{value}"
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
